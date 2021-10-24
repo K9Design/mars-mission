7fe49652ca0187a8bbd86ae85b4b3265a9ae7808
Hagen Landsem <hagenland@gmail.com>
2021-10-24 18:08:21 +0100

    logic works. 2DO: - multiple commands in a row - edge cases - validations

--- src/Rover.js
+++ src/Rover.js
@@ -1,24 +1,27 @@
 function Rover() {
+  console.log("--- NEW ROVER ---");
   this.type = "rover";
   this.mapSize = [0, 0];
   this.position = [0, 0];
   this.bearing = "N";
   this.record = [];
 
+
   this.commandBot = (command) => {
-    console.log("command["+this.record.length+"]: "+ command);
     this.record.push(command);
+    console.log("command["+this.record.length+"]: "+ command);
 
     // init
     if (this.record.length === 1) this.mapSize = [command.split(" ")[0], command.split(" ")[1]];
     if (this.record.length === 2) {
-      this.map = [command.split(" ")[0], command.split(" ")[1]];
+      this.position = [command.split(" ")[0], command.split(" ")[1]];
       this.bearing = command.split(" ")[2];
     }
     //actions
     if (this.record.length > 2) {
       console.log("  --> "+ command);
       command.split("").forEach((direction) => this.action(direction));
+      console.log("results: "+this.output());
       return this.output();
     }
 
@@ -26,6 +29,8 @@ function Rover() {
   };
 
   this.action = (action) => {
+    console.log("     "+this.position +" "+ this.bearing+"   -> "+action );
+
     if(action === "M"){
       switch(this.bearing){
         case "N": this.position[1]++; break;
@@ -50,7 +55,7 @@ function Rover() {
         case "W": this.bearing = "N"; break;
       }
     }
-    console.log("   ["+action+"] "+this.output());
+    
   };
 
   this.output = () => {
--- src/Rover.test.js
+++ src/Rover.test.js
@@ -3,15 +3,16 @@ const {Rover} = require('./Rover')
 let bot;
 
 describe("Rover initiation", () => {
-  bot = new Rover();
+  
   test("initiate and setup map limit", () => {
-    const orders = ["5 5", "1 2 N"];
+    bot = new Rover();
+    const orders = ["5 5"];
 
     bot.commandBot(orders.shift()); // mock
   });
 
-  bot = new Rover();
   test("initiate and set position and bearing", () => {
+    bot = new Rover();
     const orders = ["5 5", "1 2 N"];
 
     bot.commandBot(orders.shift()); //mock
@@ -20,9 +21,9 @@ describe("Rover initiation", () => {
 
 });
 
-xdescribe("Rover full simple tests", () => {
-  bot = new Rover();
+describe("Rover full simple tests", () => {
   test("initiate and run sequence", () => {
+    bot = new Rover();
     const orders = ["5 5", "0 0 N", "M"];
     const reported = "0 1 N";
 
@@ -33,15 +34,24 @@ xdescribe("Rover full simple tests", () => {
 
 });
 
-xdescribe("Rover full tests", () => {
-  bot = new Rover();
+describe("Rover full tests", () => {
   test("initiate and run full example sequence", () => {
+    bot = new Rover();
     const orders = ["5 5", "1 2 N", "LMLMLMLMM"];
-    const reported = "3 3 E";
+    const reported = "1 3 N";
 
     bot.commandBot(orders.shift());
     bot.commandBot(orders.shift());
     expect( bot.commandBot( orders.shift()) ).toBe(reported);
   });
 
+  test("initiate and run full example sequence", () => {
+    bot = new Rover();
+    const orders = ["5 5", "3 3 E", "MMRMMRMRRM"];
+    const reported = "5 1 E";
+
+    bot.commandBot(orders.shift());
+    bot.commandBot(orders.shift());
+    expect( bot.commandBot( orders.shift()) ).toBe(reported);
+  });
 });
